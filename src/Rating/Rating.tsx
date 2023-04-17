7b05d59b60cf9b02def33ba062a3719b85000867
Viet Nguyen <viet34tqc@gmail.com>
2023-04-17 17:20:51 +0700

    Rating component

---
+++ src/Rating/Rating.tsx
@@ -0,0 +1,15 @@
+// How to display half star <https://ishadeed.com/article/star-rating-svg/>
+// How to hover to update rating value: each star is an radio input. When we hover on an radio, it updates rating value
+
+type Props = {
+  length: number;
+  value: number;
+  onChange: () => void;
+
+};
+
+const Rating = (props: Props) => {
+  return <div>Rating</div>;
+};
+
+export default Rating;
